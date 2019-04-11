cd4970969ae627e392c3c125969c9ab66f3267ab
Archana Jayaswal <ajayaswal@fb.com>
2019-04-10 23:31:47 -0700

    deleted image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Menu from "../components/menu"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,9 +10,6 @@ const IndexPage = () => (
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Welcome to Gemini.</h1>
     <p>A simple and easy way to do extraordinary things.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
     <Link to="/page-2/">This is a link to page 2</Link>
   </Layout>
 )
